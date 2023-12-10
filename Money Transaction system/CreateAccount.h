4b0b2d47b7c3463f92e1e2c749d5f49e3b6d0065
omar elsayed <138625022+omarEls2yed@users.noreply.github.com>
2023-12-10 13:48:03 +0200

    Update CreateAccount.h

--- Money Transaction system/CreateAccount.h
+++ Money Transaction system/CreateAccount.h
@@ -1,48 +1,17 @@
-#define _pragma
-#define _CRT_SECURE_NO_WARNINGS
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <cassert>
-#include <chrono>
-#include <climits>
-#include <cmath>
-#include <complex>
-#include <cstring>
-#include <functional>
-#include <iomanip>
 #include <iostream>
 #include <map>
-#include <numeric>
-#include <queue>
-#include <random>
-#include <set>
 #include <vector>
-#include <stack>
-#include <unordered_set>
-#include <unordered_map>
-#include <algorithm>
 #include <fstream>
-#include <bitset>
-#include "ICardPayment.h"
-#include "MasterCard.h"
-#include "OperationService.h"
-#include "Visa.h"
+#include <string>
+#include <conio.h>
+#include <stdlib.h>
+#include "AccountModel.h"
 using namespace std;
 string cinbank, cincardtype;
 class CreateAccount
 {
 public:
 	AccountModel* account = new AccountModel;
-	CreateAccount(AccountModel* account2)
-	{
-		account->Name = account2->Name;
-		account->City = account2->City;
-		account->BirthdayYear = account2->BirthdayYear;
-		account->Email = account2->Email;
-		account->Gender = account2->Gender;
-		account->PhoneNumber = account2->PhoneNumber;
-		account->Password = account2->Password;
-	}
+	CreateAccount(AccountModel* account2);
 };
 
