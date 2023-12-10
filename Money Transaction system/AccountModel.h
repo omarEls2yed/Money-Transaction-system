3ccd3f519334f562050074d989169d35576950f3
omar elsayed <138625022+omarEls2yed@users.noreply.github.com>
2023-12-10 13:45:59 +0200

    Update AccountModel.h

--- Money Transaction system/AccountModel.h
+++ Money Transaction system/AccountModel.h
@@ -1,36 +1,10 @@
-#define _pragma
-#define _CRT_SECURE_NO_WARNINGS
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <cassert>
-#include <chrono>
-#include <climits>
-#include <cmath>
-#include <complex>
-#include <cstring>
-#include <functional>
-#include <iomanip>
 #include <iostream>
 #include <map>
-#include <numeric>
-#include <queue>
-#include <random>
-#include <set>
 #include <vector>
-#include <stack>
-#include <unordered_set>
-#include <unordered_map>
-#include <algorithm>
 #include <fstream>
-#include <bitset>
-#include "AccountService.h"
-#include "CardModel.h"
-#include "CreateAccount.h"
-#include "ICardPayment.h"
-#include "MasterCard.h"
-#include "OperationService.h"
-#include "Visa.h"
+#include <string>
+#include <conio.h>
+#include <stdlib.h>
 using namespace std;
 class AccountModel
 {
@@ -43,4 +17,3 @@ public:
 	string BirthdayYear;
 	string PhoneNumber;
 };
-
