e6c3f69988a62578f74d0af498aac4a9242c33ff
omar elsayed <138625022+omarEls2yed@users.noreply.github.com>
2023-12-10 13:47:28 +0200

    Update CardModel.h

--- Money Transaction system/CardModel.h
+++ Money Transaction system/CardModel.h
@@ -1,34 +1,10 @@
-#define _pragma
-#define _CRT_SECURE_NO_WARNINGS
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <cassert>
-#include <chrono>
-#include <climits>
-#include <cmath>
-#include <complex>
-#include <cstring>
-#include <functional>
-#include <iomanip>
 #include <iostream>
 #include <map>
-#include <numeric>
-#include <queue>
-#include <random>
-#include <set>
 #include <vector>
-#include <stack>
-#include <unordered_set>
-#include <unordered_map>
-#include <algorithm>
 #include <fstream>
-#include <bitset>
-#include "CreateAccount.h"
-#include "ICardPayment.h"
-#include "MasterCard.h"
-#include "OperationService.h"
-#include "Visa.h"
+#include <string>
+#include <conio.h>
+#include <stdlib.h>
 using namespace std;
 string cinbank, cincardtype;
 class CardModel
