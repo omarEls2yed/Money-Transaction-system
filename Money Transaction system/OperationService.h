26039adeee4eea84d76db1afca91c27157e0d380
omar elsayed <138625022+omarEls2yed@users.noreply.github.com>
2023-12-15 07:27:28 +0200

    Update OperationService.h

--- Money Transaction system/OperationService.h
+++ Money Transaction system/OperationService.h
@@ -13,7 +13,7 @@ class OperationService
 public:
 	void SentMoney(string type, CardModel* yourcard, string cardnumber, string frombank, string tobank, string tocard);
 	void CollectMoney(string type, CardModel* to, string tobank);
-	void transaction_history(CardModel* card);
+	void transaction_history(string cardnumber);
 	CardModel* cincard();
 };
 
