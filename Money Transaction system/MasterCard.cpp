3d547740fcf8a7f36b09b379714c91efa2707fe3
omar elsayed <138625022+omarEls2yed@users.noreply.github.com>
2023-12-15 07:22:39 +0200

    Update MasterCard.cpp

--- Money Transaction system/MasterCard.cpp
+++ Money Transaction system/MasterCard.cpp
@@ -7,90 +7,96 @@
 #include <conio.h>
 #include <stdlib.h>
 using namespace std;
-void MasterCard::send_money(CardModel* from, CardModel* to, string frombank, string tobank, string tocardtype)
+void MasterCard::void send_money(CardModel* from, string cardnumber, string frombank, string tobank, string tocardtype)
 {
 	cout << "Enter the amount of money u want to sent" << endl;
 	cin >> amountofmoney;
-	if (S->cheak_card(from, frombank, cardtype, amountofmoney) and S->cheak_card(to, tobank, tocardtype))
+	if (S->cheak_card(from, frombank, cardtype, amountofmoney) and S->cheak_card(cardnumber, tobank, tocardtype))
 	{
 		fstream file;
 		file.open("transaction.txt", ios::app);
 		file << from->CardNumber << endl;
-		file << to->CardNumber << endl;
+		file << cardnumber << endl;
 		file << amountofmoney << endl;
 		file.close();
 		fstream file2;
 		file2.open("transaction history.txt", ios::app);
 		file2 << from->CardNumber << endl;
-		file2 << to->CardNumber << endl;
+		file2 << cardnumber << endl;
 		file2 << amountofmoney << endl;
 		file2.close();
 		AccountService* Service_Account = new AccountService;
 		int place = Service_Account->indx(from, frombank, cardtype);
 		string save;
 		if (frombank == "Banquemisr")
-		{
-			save = "masterBanquemisr.txt";
-		}
-		else if (frombank == "National Bank of Egypt")
-		{
-			save = "masterNational Bank of Egypt.txt";
+                {
+	           save = "masterBanquemisr.txt";
+                }
+                else if (frombank == "National Bank of Egypt")
+                {
+		    save = "masterNational Bank of Egypt.txt";
 		}
 		else if (frombank == "The United Bank of Egypt")
 		{
-			save = "masterThe United Bank of Egypt.txt";
+		    save = "masterThe United Bank of Egypt.txt";
 		}
-		Service_Account->updatefile(save, place, amountofmoney, '-');
+	        Service_Account->updatefile(save, place, amountofmoney, '-');
+	}
+	else {
+	        cout << "you havent this amount of monye" << endl;
 	}
 }
 void MasterCard::collect_money(CardModel* to, string tobank)
-{
-	if (S->cheak_card(to, tobank, cardtype))
 	{
-		ifstream file1;
-		file1.open("transaction.txt", ios::in);
-		int idx = 0;
-		vector <string>sv;
-		while (file1.is_open())
+		if (S->cheak_card(to,tobank,cardtype))
 		{
-			string line;
-			while (getline(file1, line))
+			ifstream file1;
+			file1.open("transaction.txt", ios::in);
+			vector <string>sv;
+			while (file1.is_open())
 			{
-				sv.push_back(line);
+				string line;
+				while (getline(file1, line))
+				{
+					sv.push_back(line);
+				}
+				file1.close();
 			}
-			file1.close();
-		}
-		for (int i = 0; i < sv.size(); i++)
-		{
-			if (sv[i] == to->CardNumber)
+			vector<string>money;
+			for (int i = 0; i < sv.size(); i++)
 			{
-				sv[i + 1] = "0";
-				idx = i + 2;
-				break;
+				if (sv[i] == to->CardNumber)
+				{
+					if (sv[i + 1] != "0") {
+						money.push_back(sv[i + 1]);
+					}
+					sv[i+1] = "0";
+				}
+			}
+			ofstream file2;
+			file2.open("transaction.txt", ios::trunc);
+			for (int i = 0; i < sv.size(); i++)
+			{
+				file2 << sv[i] << endl;
+			}
+			file2.close();
+			AccountService* Service_Account = new AccountService;
+			int place = Service_Account->indx(to, tobank, cardtype);
+			string save;
+			if (tobank == "Banquemisr")
+			{
+				save = "masterBanquemisr.txt";
+			}
+			else if (tobank == "National Bank of Egypt")
+			{
+				save = "masterNational Bank of Egypt.txt";
+			}
+			else if (tobank == "The United Bank of Egypt")
+			{
+				save = "masterThe United Bank of Egypt.txt";
+			}
+			for (int i = 0; i < money.size(); i++) {
+				Service_Account->updatefile(save, place, money[i], '+');
 			}
 		}
-		ofstream file2;
-		file2.open("transaction.txt", ios::trunc);
-		for (int i = 0; i < sv.size(); i++)
-		{
-			file2 << sv[i] << endl;
-		}
-		file2.close();
-		AccountService* Service_Account = new AccountService;
-		int place = Service_Account->indx(to, tobank, cardtype);
-		string save;
-		if (tobank == "Banquemisr")
-		{
-			save = "masterBanquemisr.txt";
-		}
-		else if (tobank == "National Bank of Egypt")
-		{
-			save = "masterNational Bank of Egypt.txt";
-		}
-		else if (tobank == "The United Bank of Egypt")
-		{
-			save = "masterThe United Bank of Egypt.txt";
-		}
-		Service_Account->updatefile(save, place, amountofmoney, '+');
 	}
-}
